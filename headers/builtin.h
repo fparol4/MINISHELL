8c45664ab256716fc0610993cc3201ee88d25fc4
Fabricio Parola <fabricio.parola@gmail.com>
2026-05-19 17:18:12 -0300

    (2026-05-19 17:18:12)

--- headers/builtin.h
+++ headers/builtin.h
@@ -5,9 +5,9 @@
 
 #define PWD_HOME "HOME"
 
+int bin_echo(char **args, t_env **env);
 int bin_cd(char **args, t_env **env);
 int bin_pwd(char **args, t_env **env);
-int bin_echo(char **args, t_env **env);
 int bin_exit(char **args, t_env **env);
 int bin_env(char **args, t_env **env);
 int bin_export(char **args, t_env **env);
--- source/modules/bultin/echo.c
+++ source/modules/bultin/echo.c
@@ -1,6 +1,44 @@
 #include "../../../headers/minishell.h"
 
-int bin_echo(char **args, t_envm **envm)
+static int f_newline(char *arg)
 {
 	int i;
+
+	if (!arg || arg[0] != '-' || arg[1] == '\0')
+		return (0);
+	i = 1;
+	while (arg[i])
+		if (arg[i++] != 'n')
+			return (0);
+	return (1);
+}
+
+int bin_echo(char **args, t_env **env)
+{
+	int i;
+	int nl;
+	(void) env;
+
+	if (!args)
+	{
+		ft_putchar_fd('\n', 1);
+		return (0);
+	}
+	i = 1;
+	nl = 1;
+	while (args[i] && f_newline(args[i]))
+	{
+		nl = 0;
+		i++;
+	}
+	while (args[i])
+	{
+		ft_putstr_fd(args[i], 1);
+		if (args[i + 1])
+			ft_putstr_fd(" ", 1);
+		i++;
+	}
+	if (nl)
+		ft_putchar_fd('\n', 1);
+	return (0);
 }
