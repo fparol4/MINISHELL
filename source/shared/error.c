c627f53ea4f88da3ccf395fd9522f36dfe065f5f
GA18 <gabrielalves199816@gmail.com>
2026-06-09 09:23:19 -0300

    Corrigindo erro de norminette

--- source/shared/error.c
+++ source/shared/error.c
@@ -55,7 +55,8 @@ void	sh_err2(char *scope, char *arg, char *msg)
 void	sh_stxerr(t_syntax_error type)
 {
 	if (type == SNTX_UNCLOSED_QUOTE)
-		ft_putstr_fd("minishell: syntax error: unclosed quote\n", STDERR_FILENO);
+		ft_putstr_fd("minishell: syntax error: unclosed quote\n",
+			STDERR_FILENO);
 	else if (type == SNTX_PIPE_START)
 		ft_putstr_fd("minishell: syntax error: pipe at start\n", STDERR_FILENO);
 	else if (type == SNTX_PIPE_END)
@@ -63,5 +64,6 @@ void	sh_stxerr(t_syntax_error type)
 	else if (type == SNTX_EMPTY_CMD)
 		ft_putstr_fd("minishell: syntax error: empty command\n", STDERR_FILENO);
 	else if (type == SNTX_REDIR_NO_TARGET)
-		ft_putstr_fd("minishell: syntax error: redirection target\n", STDERR_FILENO);
+		ft_putstr_fd("minishell: syntax error: redirection target\n",
+			STDERR_FILENO);
 }
